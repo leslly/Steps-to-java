75975f7d72331272727794fdbfa8a2a9c57f29dd
Akinrinde Leslie Teniola <87911912+leslly@users.noreply.github.com>
2021-10-12 12:30:54 +0100

    Create task3.java

---
+++ task3.java
@@ -0,0 +1,21 @@
+public class Main {
+  //  Write a program to manipulate string reference type. The program should accomplish the following:
+
+   // Declare/define two strings
+   // Assign values to the strings
+  //  Concatenate the two string using a plus operator and show the output
+  //  Print the second character of the first string
+ //   Convert the first string into all uppercase
+    //Hint: Use any appropriate functions to aid your task.
+    public static void main(String[] args){
+
+        int age = 16;
+        String s = "i am ";
+        String p = "years old";
+
+        System.out.println(s.toUpperCase(Locale.forLanguageTag("i am")));
+        System.out.printf(s + age + p);
+
+    }
+
+}
