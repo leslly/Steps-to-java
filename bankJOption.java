2ffc15a77b7c1ec799ea3bf0a1e183f8a55f4a39
Akinrinde Leslie Teniola <87911912+leslly@users.noreply.github.com>
2021-10-13 13:27:03 +0100

    Create bankJOption.java

---
+++ bankJOption.java
@@ -0,0 +1,20 @@
+/* Consider a bank that offers four different types of account ( A, B, C, X).
+The followig table illustrates the annual rate of interest offered
+for each type of account. Design and implement a program that allows the user to enter an amount of money and a type of bank acount,
+before displaying the amount of money that can be earned in one year as interest on that money given tyype of bank account.
+Use any control statements you hav learntand make your program GUI based*/
+
+// Account    - Annual rate of interest
+// A          - 1.5
+// B          - 2
+// C          - 1.5
+// X          - 5
+
+//will be edited on short notice
+public class Bank {
+  
+  public static void bank(String[] agrs){
+    
+  }
+  
+}
