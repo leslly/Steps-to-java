2471fabcf3529540403fb00426514c3deb1eb4b1
Akinrinde Leslie Teniola <87911912+leslly@users.noreply.github.com>
2021-11-30 13:20:45 +0100

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,15 @@
+ class Rectangle {
+    public int length;
+    public int breath;
+
+    public int area(){
+        return length * breath;
+
+    }
+//make a coment
+    public int perimeter(){
+        return 2 * (length * breath);
+    }
+}
+
+/* this is the OOP Class for newrectangle*/
