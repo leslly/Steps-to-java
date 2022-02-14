34274cf55b3a32d7b9f84be562cfc14af12e2610
Akinrinde Leslie Teniola <87911912+leslly@users.noreply.github.com>
2022-02-14 08:52:47 +0100

    Create loop.java

---
+++ loop.java
@@ -0,0 +1,37 @@
+public class practice {
+    public static void main(String[]args) {
+        for (int i= 0; i<8; i++){
+            System.out.print('*');
+
+        }
+          System.out.println("  ");
+
+        for (int y=0; y<3; y++){
+            System.out.println("      **");
+        }
+
+        for (int j=0; j<2; j++){
+            System.out.println("**    **");
+        }
+
+        for (int i= 0; i<8; i++){
+            System.out.print('*');
+
+        }
+        /* ********
+                 **
+                 **
+                 **
+           **    **
+           **    **
+           ********
+           *
+           * Remember from the previous lessons that 'print' is stays on the line
+           * it works like '/t' in c.pp
+           * while 'println ' works like '/n '
+           * java works line by line but like all programming langusges it also works
+           * according to your commanding statement
+           * practice and see more about nested and how they work
+          */
+    }
+}
